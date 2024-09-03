2859819c7080fc21e2f92a438c16c2656148cf42
trkbba34 <tarekchenafi@gmail.com>
2024-09-04 00:46:47 +0100

    essentials 03

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, UserComponent], //standalone = false
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, SharedModule, TasksModule], //standalone = true
+})
+export class AppModule {}
